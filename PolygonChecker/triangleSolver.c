af7ca6ec6489114535f9520b3184be8aabdca747
KazziBears <justinadakin@gmail.com>
2024-11-15 12:50:41 -0500

    Added 'validateTriangle' function.

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -22,3 +22,28 @@ char* analyzeTriangle(int side1, int side2, int side3) {
 
 	return result;
 }
+
+bool validateTriangle(int a, int b, int c) {
+	bool validTriangle = false;
+	bool aCheck = false;
+	bool bCheck = false;
+	bool cCheck = false;
+
+	if (b + c > a) {
+		aCheck = true;
+	}
+	
+	if (a + c > b) {
+		bCheck = true;
+	}
+	
+	if (a + b > c) {
+		cCheck = true;
+	}
+
+	if (aCheck && bCheck && cCheck) {
+		validTriangle = true;
+	}
+
+	return validTriangle;
+}
--- PolygonChecker/triangleSolver.h
+++ PolygonChecker/triangleSolver.h
@@ -1,2 +1,4 @@
 #pragma once
+
 char* analyzeTriangle(int side1, int side2, int side3);
+bool validateTriangle(int side1, int side2, int side3);

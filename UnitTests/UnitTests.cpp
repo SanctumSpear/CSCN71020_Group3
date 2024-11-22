53f11a69599b320602fc8f3f753bad1316a5140c
SanctumSpear <146847788+SanctumSpear@users.noreply.github.com>
2024-11-22 17:19:47 -0500

    Add unit tests for validateTriangle and analyzeTriangle

--- UnitTests/UnitTests.cpp
+++ UnitTests/UnitTests.cpp
@@ -1,21 +1,75 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include <stdbool.h>
+
 extern "C" {
 	#include "../PolygonChecker/rectangleSolver.h"
 	#include "../PolygonChecker/utils.h"
 	#include "../PolygonChecker/triangleSolver.h"
 }
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTests
 {
-	TEST_CLASS(UnitTests)
+	TEST_CLASS(analyzeTriangleUnitTests)
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
-		{
-
+		TEST_METHOD(analyzeTriangle_ScaleneTriangle_IsScalene){
+			int side1 = 7;
+			int side2 = 10;
+			int side3 = 12;
+			char* expected = "Scalene triangle";
+			
+			Assert::AreEqual(expected, analyzeTriangle(side1, side2, side3));
 		}
+
+		TEST_METHOD(analyzeTriangle_IsoscelesTriangle_IsIsosceles) {
+			int side1 = 6;
+			int side2 = 6;
+			int side3 = 8;
+			char* expected = "Isosceles triangle";
+
+			Assert::AreEqual(expected, analyzeTriangle(side1, side2, side3));
+		}
+
+		TEST_METHOD(analyzeTriangle_EquilateralTriangle_IsEquilateral) {
+			int side1 = 1;
+			int side2 = 1;
+			int side3 = 1;
+			char* expected = "Equilateral triangle";
+
+			Assert::AreEqual(expected, analyzeTriangle(side1, side2, side3));
+		}
+
+		TEST_METHOD(analyzeTriangle_Invalid_IsInvalid) {
+			int side1 = 100;
+			int side2 = 1;
+			int side3 = 1;
+			char* expected = "Not a triangle";
+
+			Assert::AreEqual(expected, analyzeTriangle(side1, side2, side3));
+		}
+	};
+
+	TEST_CLASS(validateTriangleUnitTests) {
+	public:
+		TEST_METHOD(validateTriangle_ValidTriangle_True) {
+			int a = 7;
+			int b = 10;
+			int c = 12;
+			bool expected = true;
+			Assert::AreEqual(expected, validateTriangle(a, b, c));
+		}
+
+		TEST_METHOD(validateTriangle_InvalidTriangle_False) {
+			int a = 7;
+			int b = 100;
+			int c = 12;
+			bool expected = false;
+			Assert::AreEqual(expected, validateTriangle(a, b, c));
+		}
+
 	};
 }
